6f513d6056be1c7e93c36c2913998382c3b2ab4c
Daniel Danilatos <daniel@helixta.com.au>
2012-11-27 21:01:47 +1100

    remove junk

--- test.hs
+++ test.hs
@@ -11,37 +11,7 @@ import Control.Monad.RWS
 import FileSystem
 import FileSystem.Real
 import FileSystem.Fake as F
-
-
-data ServiceImplementation m = (Monad m) => ServiceImplementation
-  { serviceHello :: m ()
-  , serviceGetLine :: m String
-  , servicePutLine :: String -> m ()
-  }
-
-serviceHelloBase :: (Monad m) => ServiceImplementation m -> m ()
-serviceHelloBase impl = do
-    name <- serviceGetLine impl
-    servicePutLine impl $ "Hello, " ++ name
-
-realImpl :: ServiceImplementation IO
-realImpl = ServiceImplementation
-  { serviceHello = serviceHelloBase realImpl
-  , serviceGetLine = getLine
-  , servicePutLine = putStrLn
-  }
-
-mockImpl :: (Monad m, MonadReader String m, MonadWriter String m) =>
-    ServiceImplementation m
-mockImpl = ServiceImplementation
-  { serviceHello = serviceHelloBase mockImpl
-  , serviceGetLine = ask
-  , servicePutLine = tell
-  }
-
---main = serviceHello realImpl
---test = case runRWS (serviceHello mockImpl) "Dave" () of
---    (_, _, "Hello, Dave") -> True; _ -> False
+import ObjectStore
 
 
 doStuff :: (Monad m) => FileSystem m -> m ()
